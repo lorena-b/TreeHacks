b21d017d9f15d5959adfdee9fd168f954acc066e
lorena-b <46202743+lorena-b@users.noreply.github.com>
2021-02-13 14:44:26 -0500

    Create basis.py

---
+++ server/basis.py
@@ -0,0 +1,58 @@
+import datetime
+from datetime import date
+import auth
+
+# print(f"{value:%Y-%m-%d %H:%M:%S}")
+
+reddit = auth.reddit
+
+keyword = ('Trump')
+
+thisdict = {
+    "stocks": ["wallstreetbets", "stocks", "investing", "all", "news", "robinhood"],
+    "news": ["news", "worldnews", "usnews", "politics", "todayilearned", "all"],
+    "media": ["movies", "marvelstudios", "DC_Cinematic", "StarWars", "Music", "all"],
+    "all": ["all"]
+}
+
+valuedicmonth = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0]
+
+valuedicweek = [0, 0, 0, 0, 0, 0, 0]
+today = date.today()
+month = (int(f"{today:%m}"))
+day = (int(f"{today:%d}"))
+
+
+def monthf(keyword, subreddit, valuedic):
+    subreddit = reddit.subreddit(subreddit)
+    hot_python = subreddit.top('month', limit=100)
+    for submission in hot_python:
+        timestamp = submission.created_utc
+        value = datetime.datetime.fromtimestamp(timestamp)
+        postmonth = (int(f"{value:%m}"))
+        postday = (int(f"{value:%d}"))
+        if keyword in submission.title:
+            if postmonth == month:
+                valuedic[30 - (day - postday)] += 1
+                print("hey")
+            else:
+                print(postday)
+                if ((postmonth == 1) or (postmonth == 3) or (postmonth == 5) or
+                        (postmonth == 7) or (postmonth == 8) or
+                        (postmonth == 9) or (postmonth == 10) or (postmonth == 12)):
+                    valuedic[(day - 1) - (31 - postday) + 5] += 1
+                elif postmonth == 2:
+                    valuedic[(day - 1 - (28 - postday)) + 5] += 1
+                else:
+                    valuedic[(day - 1 - (30 - postday)) + 5] += 1
+    return valuedic
+
+
+def montharray(keyword, topic, valuedic):
+    for i in (thisdict[topic]):
+        valuedic = monthf(keyword, i, valuedic)
+    return valuedic
+
+
+print(montharray(keyword, "news", valuedicmonth))
