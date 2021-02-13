8883856b3ee7de27b914f460f69bf362b2fd7a57
lorena-b <46202743+lorena-b@users.noreply.github.com>
2021-02-12 22:19:47 -0500

    .

--- server/auth.py
+++ server/auth.py
@@ -1,19 +1,25 @@
 from dotenv import load_dotenv
-import praw 
-import os 
+import praw
+import os
 
 load_dotenv()
 
 # KEYS
-CLIENT_ID= os.environ('CLIENT_ID')
-CLIENT_SECRET= os.environ('REDDIT_SECRET')
-USERNAME= os.environ('user')
-PASSWORD= os.environ('pass')
-
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('REDDIT_SECRET')
+USERNAME = os.environ.get('user')
+PASSWORD = os.environ.get('pass')
 
 auth = praw.Reddit(client_id=CLIENT_ID,
                    client_secret=CLIENT_SECRET,
                    username=USERNAME,
                    password=PASSWORD,
                    user_agent='hackathon')
+print(reddit.auth.url(["identity"], "...", "permanent"))
 
+subreddit = reddit.subreddit("python")
+
+hot_python = subreddit.hot(limit=5)
+
+for item in hot_python:
+    print(item)
